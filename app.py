b80e6aa56c298b2c53374985b4df5eb230024f36
Dhruwang Akbari <dhruwang.akbari@gmail.com>
2024-07-09 03:35:14 +0000

    [ADD] Pokemon Classifier and navigation

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from classifier import pokemon_classifier
+from pokedex import pokedex
+
+# Sidebar
+with st.sidebar:
+    pg = st.navigation([
+        st.Page(pokedex, title="Pokedex", icon="📱"),
+        st.Page(pokemon_classifier, title="Pokemon Classificator", icon="🔍"),
+    ])
+
+pg.run()
